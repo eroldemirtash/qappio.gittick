9ae7765d8a4ca022e025606894c1c1374b6d583b
Erol Demirtaş <erldemirtas@gmail.com>
2025-09-01 12:48:49 +0300

    chore: remove legacy admin backups and ignore

--- qappion/admin_old.1756713499/next.config.ts
+++
@@ -1,6 +0,0 @@
-import type { NextConfig } from "next";
-const config: NextConfig = {
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: true }
-};
-export default config;
--- qappion/admin_old.1756713499/src/lib/cn.ts
+++
@@ -1,4 +0,0 @@
-import { type ClassValue } from "clsx";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-export function cn(...i: ClassValue[]) { return twMerge(clsx(i)); }
